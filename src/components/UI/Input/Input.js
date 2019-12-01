37236f998b538fbe756a7f3e20f0c4bffc8bb966
JakubLukaszuk <jakub_lukaszuk@wp.pl>
2019-12-02 00:40:20 +0100

    Add SingnUp and Input
    SingnUp not complete yet. Input not styled yet.

---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import classes from './Input.css';
+
+const input = (props) => {
+    let inputElement = null;
+    const inputElementClasses = [classes.inputElement];
+
+    if(props.invalid && props.toutched){
+        inputElementClasses.push(classes.Invalid);
+    }
+
+    switch(props.elementType){
+        case('input'):
+            inputElement =(
+                <input
+                    className = {inputElementClasses.join(' ')}
+                    {...props.elementConfig}
+                    value={props.value}
+                    onChange={props.changed}
+                />);
+             break;
+        case('textarea'):
+            inputElement = (
+                <textarea
+                    className = {inputElementClasses.join(' ')}
+                    {...props.elementConfig}
+                    value={props.value}
+                    onChange={props.changed}
+                />);
+              break;
+        default:
+            inputElement = <input
+                className = {inputElementClasses.join(' ')}
+                {...props.elementConfig}
+                value={props.value}
+                onChange={props.changed}
+              />;
+    }
+    return(
+        <div className = {classes.Input}>
+            <label className = {classes.Label}>
+            {props.label}</label>
+            {inputElement}
+        </div>);
+};
+
+export default input;
