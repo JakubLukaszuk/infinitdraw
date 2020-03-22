a588fbdd1b1a4c0919aa072243b99cc56bf92ee7
JakubLukaszuk <41912745+JakubLukaszuk@users.noreply.github.com>
2020-03-23 00:34:11 +0100

    Singup link, Arrow Button and styls improvment
    Arrow Button component added. Singup link added to landing page. Works only for unauthenticated users.

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,28 +1,62 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
+
 import style from './LandingPage.module.sass';
-import TextFrame from '../UI/TextFrame/TextFrame'
+import * as ROUTES from '../../constants/routes';
+import TextFrame from '../UI/TextFrame/TextFrame';
+import ArrowButton from '../UI/ArrowButton/ArrowButton';
+import {AuthUserContext} from '../../components/Session';
 
-const Landing = () => (
-  <div className={style.wrapper}>
-    <div className={style.head}>
-      <div className={style.neon}>
-        <h1>
-          Infinidraw
-        </h1>
-      </div>
-      <h2>
-        Try your luck!
-      </h2>
-    </div>
-    <div className={style.gameInfo}>
-      <div className ={style.textContainer}>
-        <TextFrame title="About game">
-          Infini draw is amazing slot machine game type. You can play on phone or
-          computer. Just needed Internet browser.
-        </TextFrame>
-      </div>
-    </div>
-  </div>
-);
 
-export default Landing;
+const Landing = () => {
+  return (
+    <AuthUserContext.Consumer>
+      {authUser => (<LandingPageBase authUser={authUser}/>)}
+    </AuthUserContext.Consumer>
+  )
+}
+
+const LandingPage = (props) => {
+
+  const scrollToBottom = () => {
+    window.scrollTo({ top: document.body.offsetHeight, behavior: 'smooth' })
+  }
+
+  const goToSignUp = () => {
+    if(!props.authUser)
+    {
+      props.history.push(ROUTES.SIGN_UP);
+    }
+  }
+
+  return (
+    <div className={style.wrapper}>
+      <div className={style.head}>
+        <div className={style.neon}>
+          <h1>
+            Infinidraw
+          </h1>
+        </div>
+        <h2>
+          Try your luck!
+        </h2>
+        <ArrowButton clicked={scrollToBottom} arrowType='down' top='80%'/>
+      </div>
+      <div className={style.gameInfo}>
+        <div className ={style.textContainer}>
+          <TextFrame title="About game">
+            Infini draw is amazing slot machine game type. You can play on phone or
+            computer. Just needed Internet browser.
+          </TextFrame>
+        </div>
+      </div>
+      <div className = {style.signUp} onClick = {goToSignUp}>
+        Signup Here
+      </div>
+    </div>
+  )
+
+};
+const LandingPageBase = withRouter(LandingPage);
+
+export default withRouter(Landing);
